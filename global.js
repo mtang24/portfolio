530f4ddbec53182c98e797db04418b3f9b0f58e3
mtang24 <michaeljtang18@gmail.com>
2025-01-23 19:56:09 -0800

    Update js urls

--- global.js
+++ global.js
@@ -22,7 +22,7 @@ for (let p of pages) {
 
   const ARE_WE_HOME = document.documentElement.classList.contains('home');
   if (!ARE_WE_HOME && !url.startsWith('http')) {
-    url = '../' + url;
+    url = '/portfolio/' + url;
   }
 
   // Create link and add it to nav
