266369317876fb2ee78fade6e5696bed1c8ae2c9
mtang24 <michaeljtang18@gmail.com>
2025-01-30 08:47:32 -0800

    Update home page js with projects and stats info

--- global.js
+++ global.js
@@ -79,3 +79,40 @@ select.addEventListener('input', function (event) {
 //   // or if (currentLink !== undefined)
 //   currentLink.classList.add('current');
 // }
+
+export async function fetchJSON(url) {
+  try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      if (!response.ok) {
+          throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      console.log(response);
+      const data = await response.json();
+      return data; 
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  // write javascript that will allow dynamic heading levels based on previous function
+  containerElement.innerHTML = '';
+  project.forEach(project => {
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+    <h3>${project.title}</h3>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.year}</p>
+    <p>${project.description}</p>
+    `;
+
+    containerElement.appendChild(article);
+  });
+}
+
+export async function fetchGitHubData(username) {
+  // return statement here
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
---
+++ projects/projects.js
@@ -0,0 +1,15 @@
+import { fetchJSON, renderProjects } from '../global.js';
+const projects = await fetchJSON('../lib/projects.json');  // Fetch the projects
+const projectsContainer = document.querySelector('.projects');  // Get the container element
+
+// Render the projects
+renderProjects(projects, projectsContainer, 'h2');
+
+// Update the projects count in the <h1>
+const projectCount = projects.length;  // Count the projects
+const header = document.querySelector('h1.projects-title');  // Find the <h1> element
+header.textContent = ` My Projects (${projectCount})`;  // Update the <h1> to display count
+
+
+
+
